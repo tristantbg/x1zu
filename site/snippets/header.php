0e0223c4d8b98fbac573a8036e7d964daf6aa426
tristanb <tristan.bagot@gmail.com>
2016-05-11 19:26:19 +0200

    SEO header improved

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,13 +4,34 @@
 
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="dns-prefetch" href="//www.google-analytics.com">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+	<link rel="canonical" href="<?php echo html($page->url()) ?>" />
+	<?php if($page->isHomepage()): ?>
+      <title><?php echo $site->title()->html() ?></title>
+    <?php else: ?>
+      <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+    <?php endif ?>
 	<meta name="description" content="<?php echo $site->description()->html() ?>">
+	<meta name="robots" content="index,follow" />
 	<meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-	<meta property="og:url" content="<?php echo $site->homePage()->url() ?>"/>
-	<meta property="og:description" content="<?php echo $site->text()->html() ?>"/>
-	<meta property="og:image" content="<?= url('assets/images/og_image.jpg') ?>"/>
+	<meta name="DC.Title" content="<?php echo $page->title()->html() ?>" />
+    <meta name="DC.Description" content="<?php echo $page->description()->html() ?>"/ >
+    <?php if($page->isHomepage()): ?>
+      <meta property="og:title" content="<?php echo $site->title()->html() ?>" />
+    <?php else: ?>
+      <meta property="og:title" content="<?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?>" />
+    <?php endif ?>
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="<?php echo html($page->url()) ?>" />
+    <meta property="og:image" content="<?= url('assets/images/og_image.jpg') ?>" />
+    <meta property="og:description" content="<?php echo $page->description()->html() ?>" />
+    <?php if($page->isHomepage()): ?>
+      <meta itemprop="name" content="<?php echo $site->title()->html() ?>">
+    <?php else: ?>
+      <meta itemprop="name" content="<?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?>">
+    <?php endif ?>
+    <meta itemprop="description" content="<?php echo $site->description()->html() ?>">
 	<link rel="shortcut icon" href="<?= url('assets/images/favicon.ico') ?>">
 	<link rel="icon" href="<?= url('assets/images/favicon.ico') ?>" type="image/x-icon">
 
